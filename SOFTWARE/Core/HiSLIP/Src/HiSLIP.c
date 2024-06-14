90fb0feec748f0243e78b876021b91253f7fdb61
BehrensG <grzegorz.behrens@gmail.com>
2024-06-14 10:26:17 +0200

    A new try

--- SOFTWARE/Core/HiSLIP/Src/HiSLIP.c
+++ SOFTWARE/Core/HiSLIP/Src/HiSLIP.c
@@ -90,44 +90,9 @@ void hislip_CopyMemory(char* destination, void** sources, size_t* sizes, unsigne
 // ----------------------------------------------------------------------------
 
 
-err_t hislip_DataEnd(hislip_instr_t* hislip_instr)
+void hislip_DataEnd(hislip_instr_t* hislip_instr)
 {
-	err_t err = 0;
 
-	hislip_msg_t msg_rx;
-
-	char* buf;
-	char* end;
-
-	const char* ends[3] = {LINE_ENDING_CR, LINE_ENDING_LF, LINE_ENDING_CRLF};
-
-	msg_rx = hislip_MsgParser(hislip_instr);
-
-	buf = &hislip_instr->netbuf.data[sizeof(hislip_msg_t)];
-
-	for(unsigned char i = 0; i < 3; i++)
-	{
-		end = strstr(buf, ends[i]);
-		if(NULL != end)
-		{
-			break;
-		}
-
-	}
-
-	if(NULL != end)
-	{
-		memcpy(end, SCPI_LINE_ENDING, strlen(SCPI_LINE_ENDING));
-	}
-	else
-	{
-		memcpy(buf + msg_rx.payload_len.lo, SCPI_LINE_ENDING, strlen(SCPI_LINE_ENDING));
-	}
-
-	//SCPI_Input(&scpi_context, buf, msg_rx.payload_len.lo + strlen(SCPI_LINE_ENDING));
-
-
-	return err;
 }
 
 /*
@@ -427,196 +392,59 @@ static NX_TCP_SOCKET g_tcp_sck[APP_TCP_SOCKET_NUM];
 
 static ULONG g_not_listening = TX_FALSE;
 
-static void g_tcp_sck_listen_cb(NX_TCP_SOCKET *, UINT);
-static void g_tcp_sck_receive_cb(NX_TCP_SOCKET *);
-static void g_tcp_sck_disconn_cb(NX_TCP_SOCKET *);
 
-/* TCP Thread entry function */
-void tcp_thread_entry(void)
+
+#define SERVER_PORT 4880
+#define MAX_CLIENTS 10
+NX_IP my_ip;
+NX_PACKET_POOL my_pool;
+NX_TCP_SOCKET server_socket;
+TX_THREAD client_threads[MAX_CLIENTS];
+NX_TCP_SOCKET client_sockets[MAX_CLIENTS];
+
+
+
+hislip_instr_t hislip_instr;
+
+void server_thread_entry(ULONG thread_input)
 {
     UINT status;
+    ULONG client_index = 0;
 
-    /* Create all sockets */
-    for (INT i = 0; i < APP_TCP_SOCKET_NUM; i++)
-    {
-        status = nx_tcp_socket_create(&NetXDuoEthIpInstance, g_tcp_sck + i, "TCP Socket",
-                                      NX_IP_NORMAL, NX_FRAGMENT_OKAY,
-                                      NX_IP_TIME_TO_LIVE, 512, NX_NULL,
-                                      g_tcp_sck_disconn_cb);
-        if (NX_SUCCESS != status)
-        {
-            __BKPT(0);
-        }
-    }
-
-#if APP_TCP_RTT_LOG
-    SEGGER_RTT_printf(0, "Created %u TCP socket%s on port %lu\n",
-                      APP_TCP_SOCKET_NUM, APP_TCP_SOCKET_NUM > 1 ? "s" : "",
-                      APP_TCP_PORT);
-#endif
-
-    /* Start listening on the first socket */
-    status = nx_tcp_server_socket_listen(&NetXDuoEthIpInstance, HISLIP_PORT, g_tcp_sck, 0,
-                                         g_tcp_sck_listen_cb);
-    if (NX_SUCCESS != status)
-    {
-        //__BKPT(0);
-    	tx_thread_sleep(2);
-    }
+    // Create and bind the server socket
+    nx_tcp_socket_create(&my_ip, &server_socket, "Server Socket", NX_IP_NORMAL, NX_FRAGMENT_OKAY, NX_IP_TIME_TO_LIVE, 512, NX_NULL, NX_NULL);
+    nx_tcp_server_socket_listen(&my_ip, SERVER_PORT, &server_socket, MAX_CLIENTS, NULL);
 
     while (1)
     {
-#if APP_TCP_INSTANT_ECHO
-        /* Everything else is handled from TCP callbacks dispatched from
-         * IP instance thread */
-        tx_thread_suspend(tx_thread_identify());
-#else
-        /* Receive pointers to TCP socket and network packet from
-         * TCP callback */
-        ULONG msg[2];
-        status = tx_queue_receive(&g_tcp_q, msg, TX_WAIT_FOREVER);
-
-        if (TX_SUCCESS == status)
+        // Accept a client connection
+        if (nx_tcp_server_socket_accept(&server_socket, NX_WAIT_FOREVER) == NX_SUCCESS)
         {
-            NX_TCP_SOCKET * p_sck    = (NX_TCP_SOCKET *) msg[0];
-            NX_PACKET     * p_packet = (NX_PACKET *)     msg[1];
+            // Create a new client socket and thread for each connection
+            nx_tcp_socket_create(&my_ip, &client_sockets[client_index], "Client Socket", NX_IP_NORMAL, NX_FRAGMENT_OKAY, NX_IP_TIME_TO_LIVE, 512, NX_NULL, NX_NULL);
+            nx_tcp_server_socket_unaccept(&server_socket);
+            nx_tcp_server_socket_relisten(&my_ip, SERVER_PORT, &server_socket);
 
-            status = nx_tcp_socket_send(p_sck, p_packet, NX_NO_WAIT);
-            if (NX_SUCCESS != status)
+            // Start the client thread
+            if(0 == client_index){
+            tx_thread_create(&client_threads[client_index], "Client Thread", sync_thread_entry, (ULONG)&client_sockets[client_index],
+            		sync_thread_stack, HISLIP_THREAD_STACKSIZE, 1, 1, TX_NO_TIME_SLICE, TX_AUTO_START);
+            }else if(0 == client_index)
             {
-                nx_packet_release(p_packet);
+                tx_thread_create(&client_threads[client_index], "Client Thread", async_thread_entry, (ULONG)&client_sockets[client_index],
+                		async_thread_stack, HISLIP_THREAD_STACKSIZE, 1, 1, TX_NO_TIME_SLICE, TX_AUTO_START);
             }
+
+            // Increment client index, wrap around if necessary
+            client_index = (client_index + 1) % MAX_CLIENTS;
         }
-
-        else
-        {
-            /* Unrecoverable error */
-            tx_thread_suspend(tx_thread_identify());
-        }
-#endif
-    }
-}
-
-
-static void g_tcp_sck_listen_cb(NX_TCP_SOCKET * p_sck, UINT port)
-{
-    /* Incoming connection, accept and queueing new requests */
-    nx_tcp_server_socket_accept(p_sck, NX_NO_WAIT);
- //   nx_tcp_server_socket_unlisten(&NetXDuoEthIpInstance, port);
-    nx_tcp_socket_receive_notify(p_sck, g_tcp_sck_receive_cb);
-
-#if APP_TCP_RTT_LOG
-    ULONG ip, client_port;
-    nx_tcp_socket_peer_info_get(p_sck, &ip, &client_port);
-    SEGGER_RTT_printf(0, "[TCP %x] Incoming connection from %lu.%lu.%lu.%lu:%lu\n",
-                      p_sck, (ip >> 24) & 0xFF, (ip >> 16) & 0xFF,
-                      (ip >> 8) & 0xFF, (ip) & 0xFF, client_port);
-#endif
-
-    /* Attempt to find another idle socket to start listening on */
-    ULONG state = 0;
-
-    for (INT i = 0; i < APP_TCP_SOCKET_NUM; i++)
-    {
-        /* Get socket state value */
-        nx_tcp_socket_info_get(g_tcp_sck + i,
-                               0, 0, 0, 0, 0, 0, 0, &state, 0, 0, 0);
-
-        /* Start lisnening if socket is idle */
-        if (NX_TCP_CLOSED == state)
-        {
-            nx_tcp_server_socket_listen(&NetXDuoEthIpInstance, port, g_tcp_sck + i, 0,
-                                        g_tcp_sck_listen_cb);
-
-            break;
-        }
-    }
-
-    /* Ran out of sockets, set appropriate flag to let next socket to
-     * disconnect know that it should start listening right away. */
-    if (NX_TCP_CLOSED != state)
-    {
-        g_not_listening = TX_TRUE;
-    }
-}
-hislip_instr_t hislip_instr;
-
-static void g_tcp_sck_receive_cb(NX_TCP_SOCKET * p_sck)
-{
-    NX_PACKET * p_packet;
-
-    /* This callback is invoked when data is already received. Retrieving
-     * packet with no suspension. */
-  //  nx_tcp_socket_receive(p_sck, &p_packet, NX_NO_WAIT);
-
-#if APP_TCP_RTT_LOG
-    ULONG ip, client_port;
-    nx_tcp_socket_peer_info_get(p_sck, &ip, &client_port);
-    SEGGER_RTT_printf(0, "[TCP %x] Incoming packet (%lu bytes) from %lu.%lu.%lu.%lu:%lu\n",
-                      p_sck, p_packet->nx_packet_length, (ip >> 24) & 0xFF,
-                      (ip >> 16) & 0xFF, (ip >> 8) & 0xFF, (ip) & 0xFF, client_port);
-#endif
-   // hislip_instr.packet = p_packet;
-    hislip_instr.socket = p_sck;
-#if APP_TCP_INSTANT_ECHO
-    /* Send packet back on the same TCP socket */
-    switch(hislip_Recv(&hislip_instr))
-    		{
-    			case Initialize : hislip_Initialize(&hislip_instr);break;
-    			case AsyncInitialize : hislip_AsyncInitialize(&hislip_instr); break;
-    			case AsyncMaximumMessageSize : hislip_AsyncMaximumMessageSize(&hislip_instr);break;
-    			case DataEnd : hislip_DataEnd(&hislip_instr); break;
-
-    			case HISLIP_CONN_ERR :
-    				{
-    			        nx_tcp_socket_disconnect(hislip_instr.socket, NX_WAIT_FOREVER);
-    			        nx_tcp_server_socket_unaccept(&hislip_instr.socket);
-    			        nx_tcp_server_socket_relisten(&NetXDuoEthIpInstance, HISLIP_PORT, &hislip_instr.socket);
-
-    					//vTaskDelete(NULL);
-    					HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-
-    				}; break;
-
-    			default : tx_thread_sleep(2); break;
-    		}
-#else
-    ULONG msg[2] =
-    {
-        (ULONG) p_sck, (ULONG) p_packet
-    };
-
-    /* Sent TCP socket and packet pointers to user thread */
-    if (TX_SUCCESS != tx_queue_send(&g_tcp_q, msg, TX_NO_WAIT))
-    {
-        nx_packet_release(p_packet);
-    }
-#endif
-}
-
-
-static void g_tcp_sck_disconn_cb(NX_TCP_SOCKET * p_sck)
-{
-#if APP_TCP_RTT_LOG
-    SEGGER_RTT_printf(0, "[TCP %x] Client disconnected\n", p_sck);
-#endif
-
-    nx_tcp_server_socket_unaccept(p_sck);
-
-    /* If all sockets are busy, start listening again */
-    if (TX_TRUE == g_not_listening)
-    {
-        nx_tcp_server_socket_listen(&NetXDuoEthIpInstance, HISLIP_PORT, p_sck, 0,
-                                    g_tcp_sck_listen_cb);
-
-        g_not_listening = TX_FALSE;
     }
 }
 
 void hislip_CreateTask(void)
 {
 
-	tx_thread_create(&server_thread, "HiSLIP Server Thread", tcp_thread_entry, 0,
+	tx_thread_create(&server_thread, "HiSLIP Server Thread", server_thread_entry, 0,
 	                     server_thread_stack, HISLIP_THREAD_STACKSIZE, 1, 1, TX_NO_TIME_SLICE, TX_AUTO_START);
 
 
